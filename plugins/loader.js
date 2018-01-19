800bcc1a36f3099a3540d2141e0632fe5fb50df2
Muthu Kumar <muthukumar@thefeathers.in>
2018-01-19 18:27:36 +0530

    Update plugins/loader.js

--- plugins/loader.js
+++ plugins/loader.js
@@ -1,10 +1,12 @@
 'use strict'
 
+const  { EOL } = require('os')
+
 function plugins() {
 	const { plugins } = require('../config')
 	plugins.forEach(plugin => {
 		require('../plugins/' + plugin + '/main.js')
-		console.log(`Loaded plugin ${plugin}...`)
+		console.log(EOL + 'Loaded plugin ' + plugin + '...')
 	})
 }
 
