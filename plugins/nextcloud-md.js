e9b881872b029fb09c52a54cba331fa7e8df078a
Muthu Kumar <muthukumar@thefeathers.in>
2019-03-20 00:07:22 +0530

    [fix] Only redirect if it's not already index.php

--- plugins/nextcloud-md.js
+++ plugins/nextcloud-md.js
@@ -1,1 +1,4 @@
-module.exports = app => app.use((req, res, next) => res.redirect('/index.php' + req.originalUrl));
+module.exports = app => app.use((req, res, next) =>
+	req.originalUrl.includes('index.php')
+		? next()
+		: res.redirect('/index.php' + req.originalUrl));
